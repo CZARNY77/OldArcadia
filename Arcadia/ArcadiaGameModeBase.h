8fd1c4c08c878c33ec6581fc692c4b1f0def7373
CZARNY <60876248+CZARNY77@users.noreply.github.com>
2021-08-23 14:48:15 +0200

    1

---
+++ Arcadia/ArcadiaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcadiaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADIA_API AArcadiaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
