77c8aa61b009fae0b64df1aacbdb13952511a2b6
CZARNY <60876248+CZARNY77@users.noreply.github.com>
2021-08-24 15:06:39 +0200

    3

--- Arcadia/Gracz.cpp
+++ Arcadia/Gracz.cpp
@@ -12,13 +12,17 @@ AGracz::AGracz()
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("ArmCamera"));
 	SpringArm->SetupAttachment(RootComponent);
-	SpringArm->SocketOffset = FVector(0.f, 0.f, 0.f);
+	SpringArm->TargetArmLength = 0.f;
+	SpringArm->SocketOffset = FVector(0.f, 0.f, 60.f);
 	SpringArm->bEnableCameraLag = true;
-	SpringArm->CameraLagSpeed = 15.f;
+	SpringArm->CameraLagSpeed = 40.f;
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(RootComponent);
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 	Camera->bUsePawnControlRotation = true;
+
+	MaxSpeed = 300.f;
+	pJump = false;
 }
 
 void AGracz::BeginPlay()
@@ -29,17 +33,36 @@ void AGracz::BeginPlay()
 
 void AGracz::MoveRight(float Val)
 {
-
+	if (Val > 0.2f || Val < -0.2f) {
+		float NewSpeed = Speed + (GetWorld()->GetDeltaSeconds() * (Val * 100.f));
+		Speed = FMath::Clamp(NewSpeed, -MaxSpeed, MaxSpeed);
+	}
+	else if(Speed != 0.f) {
+		bool bSpeed = Speed > 0.f;
+		float Slow = bSpeed ? -5 : 5;
+		float NewSpeed = Speed + (GetWorld()->GetDeltaSeconds() * (Slow * 100.f));
+		Speed = bSpeed ? FMath::Clamp(NewSpeed, 0.f, MaxSpeed) : FMath::Clamp(NewSpeed, -MaxSpeed, 0.f);
+	}
 }
 
-void AGracz::Jump(float Val)
+void AGracz::Jump()
 {
+	if (pJump) {
+		Up = 0.f;
+		pJump = false;
+	}
+	else {
+		Up = 400.f;
+		pJump = true;
+	}
 }
 
 void AGracz::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	const FVector LocalMove = FVector(0.f, Speed * DeltaTime, Up * DeltaTime);
+	AddActorLocalOffset(LocalMove, true);
 }
 
 void AGracz::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -47,6 +70,7 @@ void AGracz::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("Right", this, &AGracz::MoveRight);
-	PlayerInputComponent->BindAxis("Jump", this, &AGracz::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed ,this, &AGracz::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AGracz::Jump);
 }
 
--- Arcadia/Gracz.h
+++ Arcadia/Gracz.h
@@ -16,6 +16,14 @@ class ARCADIA_API AGracz : public APawn
 	class USpringArmComponent* SpringArm;
 	UPROPERTY(EditAnywhere, Category = "Camera")
 	class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Move")
+	float Speed;
+	UPROPERTY(EditAnywhere, Category = "Move")
+	float Up;
+
+	float MaxSpeed;
+	bool pJump;
 public:
 	AGracz();
 
@@ -23,7 +31,7 @@ protected:
 	virtual void BeginPlay() override;
 
 	void MoveRight(float Val);
-	void Jump(float Val);
+	void Jump();
 public:	
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
